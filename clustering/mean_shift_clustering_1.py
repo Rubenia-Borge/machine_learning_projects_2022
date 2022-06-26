5f3577a7049747cbf45ab84638d9473cc76f6a00
Rubenia Borge <rubeniaolaska@gmail.com>
2022-06-26 00:00:26 -0500

    Update mean_shift_clustering_1.py

--- clustering/mean_shift_clustering_1.py
+++ clustering/mean_shift_clustering_1.py
@@ -1,3 +1,6 @@
 import numpy as np
 from sklearn.cluster import MeanShift, estimate_bandwidth
 from sklearn.datasets import make_blobs
+
+centers = [[1, 1], [-1, -1], [1, -1]]
+X, _ = make_blobs(n_samples=10000, centers=centers, cluster_std=0.6)
