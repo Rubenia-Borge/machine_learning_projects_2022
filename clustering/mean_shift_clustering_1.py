1c581985c537db4a57f7fe729201334949c068cf
Rubenia Borge <rubeniaolaska@gmail.com>
2022-06-26 00:01:04 -0500

    Update mean_shift_clustering_1.py

--- clustering/mean_shift_clustering_1.py
+++ clustering/mean_shift_clustering_1.py
@@ -6,4 +6,15 @@ centers = [[1, 1], [-1, -1], [1, -1]]
 X, _ = make_blobs(n_samples=10000, centers=centers, cluster_std=0.6)
 
 
+# The following bandwidth can be automatically detected using
+bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=500)
 
+ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+ms.fit(X)
+labels = ms.labels_
+cluster_centers = ms.cluster_centers_
+
+labels_unique = np.unique(labels)
+n_clusters_ = len(labels_unique)
+
+print("number of estimated clusters : %d" % n_clusters_)
